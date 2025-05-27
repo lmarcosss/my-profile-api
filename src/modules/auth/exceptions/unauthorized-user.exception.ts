d54db60d892385225364251cf486fdfa8757f0ba
Leonardo dos Santos Marcos <leonardo.marcos@cwi.com.br>
2025-05-27 11:54:35 -0300

    Remove unauthorized-user.exception

--- src/modules/auth/exceptions/unauthorized-user.exception.ts
+++
@@ -1,7 +0,0 @@
-import { UnauthorizedException } from "@nestjs/common";
-
-export class UnauthorizedUserException extends UnauthorizedException {
-  constructor() {
-    super("User not found");
-  }
-}
