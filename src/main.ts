dbcedb1c98826267286772e9975bea137e8ec084
Leonardo dos Santos Marcos <leonardo.marcos@cwi.com.br>
2025-05-27 12:04:26 -0300

    create parseCorsOrigin in main config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { parseCorsOrigin } from "./common/utils/cors.util";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [process.env.CORS_ORIGIN],
+    origin: parseCorsOrigin(process.env.CORS_ORIGIN),
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
